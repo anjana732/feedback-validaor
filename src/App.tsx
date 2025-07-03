d38e2f6aa4d0848d98f7722d7e0d7119382a8fd3
anjana732 <anjanasingh1257@gmail.com>
2025-07-03 20:12:03 +0530

    change shadcn default theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-   import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+
 function App() {
 
 return(
   <>
+      <NextThemesProvider attribute="class" defaultTheme="dark">
+
     <h3 className='text-red-600'>Hello</h3>
     {/* <div className="flex min-h-svh flex-col items-center justify-center">
       <Button>Click me</Button>
     </div> */}
+    </NextThemesProvider>
   </>
 )
 }
