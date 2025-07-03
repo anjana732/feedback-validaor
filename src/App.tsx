292ffca0b308c32d2ed7c159d84f764540431940
anjana732 <anjanasingh1257@gmail.com>
2025-07-03 20:00:11 +0530

    Add Shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+   import { Button } from "@/components/ui/button"
 function App() {
 
 return(
   <>
     <h3 className='text-red-600'>Hello</h3>
+    {/* <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div> */}
   </>
 )
 }
