1bddbcc6acca102811bcba4fd977c3f7f15c38a0
anjana732 <anjanasingh1257@gmail.com>
2025-07-04 07:32:05 +0530

    Add popup

--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -1,12 +1,12 @@
 import { Button } from "../ui/button";
 
-export default function Header(){
+export default function Header({onAction }: { onAction: () => void }){
      return (
      <div className="w-full px-8 py-6 flex items-center gap-6">
       <p className="text-white text-3xl font-semibold">
         Click the button next to get insight for feedback.
       </p>
-      <Button >
+      <Button onClick={onAction}>
         Get Insight
       </Button>
     </div>
