e5f71bbf09666437d7539922c6ffb9fc41f51a89
anjana732 <anjanasingh1257@gmail.com>
2025-07-08 07:07:01 +0530

    Collect form data

--- src/components/layouts/feedbackForm.tsx
+++ src/components/layouts/feedbackForm.tsx
@@ -4,11 +4,12 @@ import { Button } from "@/components/ui/button";
 import FeedbackAnimation from "@/assets/lottie/feedbackAnimation.json";
 import Lottie from "lottie-react";
 import { TourProvider, useTour } from "@reactour/tour";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 // ------------------ Content Component ------------------ //
 function FeedbackFormContent() {
   const { setIsOpen, setCurrentStep } = useTour();
+  const formRef = useRef(null);
 
   useEffect(() => {
     const timeout = setTimeout(() => {
@@ -19,29 +20,38 @@ function FeedbackFormContent() {
     return () => clearTimeout(timeout);
   }, []);
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("clicked");
+    if (!formRef.current) return ;
+    const formData = new FormData(formRef.current);
+    const data = Object.fromEntries(formData.entries());
+    console.log(data);
+  }
+
   return (
     <div className="w-full px-6 lg:px-16 py-12">
       <div className="flex flex-col lg:flex-row justify-between items-start gap-12">
         {/* Feedback Form */}
         <div className="w-full lg:max-w-xl">
-          <form className="space-y-6 bg-white dark:bg-zinc-900 p-6 rounded-2xl shadow-xl h-full">
+          <form className="space-y-6 bg-white dark:bg-zinc-900 p-6 rounded-2xl shadow-xl h-full" ref={formRef} onSubmit={handleSubmit}>
             <h2 className="text-3xl font-semibold text-zinc-900 dark:text-zinc-100 text-center">
               Share your Feedback
             </h2>
-
+    
             <div className="space-y-2 step-1">
               <label className="text-sm font-medium text-zinc-700 dark:text-zinc-300">Your Name</label>
-              <Input type="text" placeholder="Enter your name" />
+              <Input type="text" name="Name" placeholder="Enter your name" />
             </div>
 
             <div className="space-y-2 step-2">
               <label className="text-sm font-medium text-zinc-700 dark:text-zinc-300">Email</label>
-              <Input type="email" placeholder="Enter your email" />
+              <Input type="email" name="Email" placeholder="Enter your email" />
             </div>
 
             <div className="space-y-2 step-3">
               <label className="text-sm font-medium text-zinc-700 dark:text-zinc-300">Your Feedback</label>
-              <Textarea placeholder="Share your thoughts..." />
+              <Textarea name="Message" placeholder="Share your thoughts..." />
             </div>
 
             <Button type="submit" className="w-full mt-4 step-4">
